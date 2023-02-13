1c5a247225712be7e3485638c135429d9d5e35ee
Vanya1000 <Vanya1000@yandex.ru>
2023-02-13 21:50:49 +0300

    feat: implement create migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DataBaseModule } from './dataBase/database.module';
 import { UsersModule } from './users/users.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavsModule } from './favs/favs.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
-import { Artist } from './artist/entities/artist.entity';
-import { Album } from './album/entities/album.entity';
-import { Track } from './track/entities/track.entity';
-import { Favorites } from './favs/entities/fav.entity';
+import { ormconfig } from './ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [User, Artist, Album, Track, Favorites],
-      logging: true,
-      synchronize: true,
-    }),
-    DataBaseModule,
+    TypeOrmModule.forRoot(ormconfig),
     UsersModule,
     ArtistModule,
     AlbumModule,
---
+++ src/ormconfig.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { User } from './users/entities/user.entity';
+import { Album } from './album/entities/album.entity';
+import { Artist } from './artist/entities/artist.entity';
+import { Track } from './track/entities/track.entity';
+import { Favorites } from './favs/entities/fav.entity';
+
+dotenv.config();
+
+export const ormconfig = {
+  type: process.env.TYPE_CONNECTION,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [User, Artist, Album, Track, Favorites],
+  migrations: ['../migrations/*{.ts,.js}'],
+  migrationsTableName: 'migrations',
+  logging: true,
+} as DataSourceOptions;
+
+export default new DataSource(ormconfig);
