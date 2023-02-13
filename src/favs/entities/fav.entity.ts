13de85d9d3b0f8daef99ce717140db4126bd3ce6
Vanya1000 <Vanya1000@yandex.ru>
2023-02-13 12:48:39 +0300

    feat: implement favorites logic

--- src/favs/entities/fav.entity.ts
+++ src/favs/entities/fav.entity.ts
@@ -1,1 +1,28 @@
-export class Fav {}
+import { Entity, PrimaryColumn, OneToMany } from 'typeorm';
+import { Artist } from './../../artist/entities/artist.entity';
+import { Album } from './../../album/entities/album.entity';
+import { Track } from './../../track/entities/track.entity';
+
+@Entity()
+export class Favorites {
+  @PrimaryColumn({ default: 1 })
+  id: number;
+
+  @OneToMany(() => Artist, (artist) => artist.favorites, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  artists: Artist[];
+
+  @OneToMany(() => Album, (album) => album.favorites, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  albums: Album[];
+
+  @OneToMany(() => Track, (track) => track.favorites, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  tracks: Track[];
+}
--- src/favs/favs.service.ts
+++ src/favs/favs.service.ts
@@ -1,17 +1,74 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { HttpStatus } from '@nestjs/common/enums';
 import { HttpException } from '@nestjs/common/exceptions';
-import DB from 'src/utils/DB/DB';
 import { TrackService } from './../track/track.service';
 import { FavsPathType } from './types/type';
 import { ArtistService } from './../artist/artist.service';
 import { AlbumService } from './../album/album.service';
+import { Favorites } from './entities/fav.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class FavsService {
-  constructor(private db: DB) {}
+  constructor(
+    @InjectRepository(Favorites)
+    private favoritesRepository: Repository<Favorites>,
+    private readonly artistService: ArtistService,
+    private readonly albumService: AlbumService,
+    private readonly trackService: TrackService,
+  ) {}
 
-  /* private getEntity(type: FavsPathType) {
+  async findAll() {
+    const { albums, artists, tracks } = await this.favoritesRepository.findOne({
+      where: { id: 1 },
+      relations: ['albums', 'artists', 'tracks'],
+    });
+    return { albums, artists, tracks };
+  }
+
+  async create(type: FavsPathType, id: string) {
+    const entity = await this.getEntityByType(type, id);
+
+    const favItem = await this.getFavoritiesByType<typeof type>(type);
+
+    const isAlreadyInFavs = favItem[type].some((item) => item.id === id);
+
+    if (isAlreadyInFavs) {
+      throw new HttpException('Already in favorites', HttpStatus.CONFLICT);
+    }
+
+    await this.favoritesRepository.save({
+      id: 1,
+      [type]: [...favItem[type], entity],
+    });
+
+    return {
+      message: `Id: ${id} successfully added to ${type}`,
+    };
+  }
+
+  async remove(type: FavsPathType, id: string) {
+    await this.getEntityByType(type, id);
+
+    const favItem = await this.getFavoritiesByType<typeof type>(type);
+
+    const isAlreadyInFavs = favItem[type].some((item) => item.id === id);
+
+    if (!isAlreadyInFavs) {
+      throw new HttpException('Not in favorites', HttpStatus.CONFLICT);
+    }
+
+    const spliceIndex = favItem[type].findIndex((item) => item.id === id);
+    favItem[type].splice(spliceIndex, 1);
+
+    await this.favoritesRepository.save({
+      id: 1,
+      [type]: favItem[type],
+    });
+  }
+
+  private getServiceByType(type: FavsPathType) {
     switch (type) {
       case 'artists':
         return this.artistService;
@@ -20,52 +77,27 @@ export class FavsService {
       case 'tracks':
         return this.trackService;
     }
-  } */
-
-  findAll() {
-    /* const favs = this.db.favs.findMany();
-    const favsWithEntities = {};
-    Object.entries(favs).forEach(([key, value]) => {
-      favsWithEntities[key] = this.getEntity(key as FavsPathType).tryGetMany(
-        value,
-      );
-    });
-    return favsWithEntities; */
   }
 
-  create(type: FavsPathType, id: string) {
-    /* const track = this.getEntity(type).tryGetOne(id);
-    if (!track) {
+  private async getEntityByType(type: FavsPathType, id: string) {
+    const entity = await this.getServiceByType(type).tryFindOne(id);
+
+    if (!entity) {
       throw new HttpException(
-        'Track not found',
+        `${type} with id=${id} not found`,
         HttpStatus.UNPROCESSABLE_ENTITY,
       );
     }
-    const entity = this.db.favs.findOneByType(type); // take out of db
-    if (entity.includes(id)) {
-      throw new HttpException('Already in favs', HttpStatus.CONFLICT);
-    }
-    this.db.favs.add(type, id);
-    return {
-      message: `Id: ${id} successfully added to ${type}`,
-    }; */
+    return entity;
   }
 
-  remove(type: FavsPathType, id: string) {
-    /* const entity = this.db.favs.findOneByType(type);
-    if (!entity.includes(id)) {
-      throw new HttpException('Not in favs', HttpStatus.NOT_FOUND);
-    }
-    this.db.favs.delete(type, id);
-    return {
-      message: `Id: ${id} successfully removed from ${type}`,
-    }; */
+  private async getFavoritiesByType<T extends keyof Favorites>(
+    type: FavsPathType,
+  ): Promise<Pick<Favorites, T>> {
+    return await this.favoritesRepository.findOne({
+      where: { id: 1 },
+      select: [type],
+      relations: [type],
+    });
   }
-
-  /* removeFromAnother(type: FavsPathType, id: string) {
-    const entity = this.db.favs.findOneByType(type);
-    if (entity.includes(id)) {
-      this.db.favs.delete(type, id);
-    }
-  } */
 }
